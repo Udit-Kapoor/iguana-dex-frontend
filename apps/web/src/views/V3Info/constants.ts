efa06c5cc634adcec585f7577870c5b0911e522c
Udit Kapoor <68367301+Udit-Kapoor@users.noreply.github.com>
2025-08-23 01:22:45 +0530

    Update constants.ts

--- apps/web/src/views/V3Info/constants.ts
+++ apps/web/src/views/V3Info/constants.ts
@@ -63,7 +63,7 @@ export const SUBGRAPH_START_BLOCK = {
   [ChainId.LINEA]: 1444,
   [ChainId.BASE]: 2912007,
   [ChainId.OPBNB]: 1721753,
-  [ChainId.ETHERLINK]: 19940245,
+  [ChainId.ETHERLINK]: 23840000,
   [ChainId.ETHERLINK_TESTNET]: 4190090,
 }
 
